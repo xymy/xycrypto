140b4a6323f5277ee3b1767310dbd602c9f9f37e
xymy <thyfan@163.com>
2020-03-10 09:48:23 +0800

    Add comments

--- xycrypto/hmac.py
+++ xycrypto/hmac.py
@@ -7,14 +7,22 @@ _TRANS_5C = bytes((x ^ 0x5C) for x in range(256))
 
 
 class HMAC(object):
-    def __init__(self, hash_class, key):
-        self.i_ctx = hash_class()
-        self.o_ctx = hash_class()
+    """Hash-based message authentication codes."""
+
+    # =================
+    # Context Interface
+    # =================
+
+    def __init__(self, hash_cls, key):
+        """Initialize the current context."""
+
+        self.i_ctx = hash_cls()
+        self.o_ctx = hash_cls()
         self.block_size = self.o_ctx.block_size
         self.digest_size = self.o_ctx.digest_size
 
         if len(key) > self.block_size:
-            ctx = hash_class()
+            ctx = hash_cls()
             ctx.update(key)
             key = ctx.finalize()
         key = key.ljust(self.block_size, b'\0')
@@ -22,18 +30,26 @@ class HMAC(object):
         self.i_ctx.update(key.translate(_TRANS_36))
         self.o_ctx.update(key.translate(_TRANS_5C))
 
-    def update(self, msg):
-        self.i_ctx.update(msg)
+    def update(self, data):
+        """Update the current context."""
+
+        self.i_ctx.update(data)
 
     def finalize(self):
+        """Finalize the current context and return the message digest as bytes."""
+
         ctx = self.o_ctx.copy()
         ctx.update(self.i_ctx.finalize())
         return ctx.finalize()
 
     def verify(self, signature):
+        """Finalize the current context and securely compare digest to signature."""
+
         return hmac.compare_digest(self.finalize(), signature)
 
     def copy(self):
+        """Copy the current context."""
+
         other = type(self).__new__(type(self))
         other.block_size = self.block_size
         other.digest_size = self.digest_size
