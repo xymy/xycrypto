a2f283fbb91fbc30f492da41037079f6dbbf17df
xymy <thyfan@163.com>
2020-03-11 12:29:53 +0800

    Update build script

--- build.py
+++ build.py
@@ -1,9 +1,19 @@
+import argparse
 import os
 
 commands = """
 python setup.py clean --all
 python setup.py build sdist bdist_wheel
 """
+
+parser = argparse.ArgumentParser()
+parser.add_argument('--tag', action='store_true')
+args = parser.parse_args()
+if args.tag:
+    from xycrypto import __version__
+    commands += """
+    git tag -a v{version} -m "xycrypto {version}"
+    """.format(version=__version__)
 
 for command in commands.splitlines():
     if command.isspace():
