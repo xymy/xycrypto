d252c5abcf017b7741fd3a4b97a38979576a1261
xymy <thyfan@163.com>
2020-03-15 14:35:36 +0800

    Improve class BlockCipher and StreamCipher

--- xycrypto/ciphers/_base.py
+++ xycrypto/ciphers/_base.py
@@ -17,11 +17,31 @@ class BlockCipher(Cipher):
         """The block size in bytes of this cipher."""
 
     def __init__(self, key, mode, padding=None):
-        self.cipher = _lib.Cipher(self._algorithm(key), mode, _lib.backend)
-        if padding is not None:
-            self.padding = padding(self.block_size)
+        self._cipher = _lib.Cipher(self._algorithm(key), mode, _lib.backend)
+        if padding is None:
+            self._padding = None
+        else:
+            self._padding = padding(self.block_size)
+
+    def encryptor(self):
+        encryptor = self._cipher.encryptor()
+        if self._padding is None:
+            return encryptor
+        return self._padding.wrap_encryptor(encryptor)
+
+    def decryptor(self):
+        decryptor = self._cipher.decryptor()
+        if self._padding is None:
+            return decryptor
+        return self._padding.wrap_decryptor(decryptor)
 
 
 class StreamCipher(Cipher):
     def __init__(self, key):
-        self.cipher = _lib.Cipher(self._algorithm(key), None, _lib.backend)
+        self._cipher = _lib.Cipher(self._algorithm(key), None, _lib.backend)
+
+    def encryptor(self):
+        return self._cipher.encryptor()
+
+    def decryptor(self):
+        return self._cipher.decryptor()
