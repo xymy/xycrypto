e45d6e9f3637185909451bf1812a5c034f2f8eb4
xymy <thyfan@163.com>
2020-03-16 13:28:23 +0800

    Improve build script

--- build.py
+++ build.py
@@ -1,21 +1,25 @@
 import argparse
 import os
 
-commands = """
-python setup.py clean --all
-python setup.py build sdist bdist_wheel
-"""
 
-parser = argparse.ArgumentParser()
-parser.add_argument('--tag', action='store_true')
-args = parser.parse_args()
-if args.tag:
-    from xycrypto import __version__
-    commands += """
-    git tag -a v{version} -m "xycrypto {version}"
-    """.format(version=__version__)
+def main():
+    parser = argparse.ArgumentParser(description='Build the xycrypto package.')
+    parser.add_argument('-t', '--tag', action='store_true', help='add git tag')
+    args = parser.parse_args()
 
-for command in commands.splitlines():
-    if command.isspace():
-        continue
-    os.system(command)
+    commands = [
+        'python setup.py clean --all',
+        'python setup.py build sdist bdist_wheel'
+    ]
+
+    if args.tag:
+        from xycrypto import __version__
+        commands.append('git tag -a v{version} -m "xycrypto {version}"'.format(
+            version=__version__))
+
+    for command in commands:
+        os.system(command)
+
+
+if __name__ == "__main__":
+    main()
