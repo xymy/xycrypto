959e49a3f949c3b856266cd989c23dbcc4a9b3a6
xymy <thyfan@163.com>
2020-03-18 11:59:17 +0800

    Update _lib

--- src/xycrypto/ciphers/_lib.py
+++ src/xycrypto/ciphers/_lib.py
@@ -1,9 +1,10 @@
-from cryptography.hazmat.backends import default_backend    # NOQA; isort:skip
-from cryptography.hazmat.primitives.ciphers import Cipher   # NOQA; isort:skip
-from cryptography.hazmat.primitives.ciphers.algorithms import (     # NOQA; isort:skip
-    ARC4, AES, TripleDES
+from cryptography.hazmat.backends import default_backend                # NOQA; isort:skip
+from cryptography.hazmat.primitives.ciphers import Cipher               # NOQA; isort:skip
+from cryptography.hazmat.primitives.ciphers.algorithms import (         # NOQA; isort:skip
+    ARC4, ChaCha20,
+    AES, Blowfish, Camellia, CAST5, IDEA, SEED, TripleDES
 )
-from cryptography.hazmat.primitives.ciphers.modes import (          # NOQA; isort:skip
+from cryptography.hazmat.primitives.ciphers.modes import (              # NOQA; isort:skip
     ECB, CBC, OFB, CFB, CTR
 )
 
