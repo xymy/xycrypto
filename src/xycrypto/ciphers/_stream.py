4178543912a519e10bd69b91ede07810dee13b0d
xymy <thyfan@163.com>
2020-03-18 12:00:55 +0800

    Support ChaCha20

--- src/xycrypto/ciphers/_stream.py
+++ src/xycrypto/ciphers/_stream.py
@@ -4,3 +4,8 @@ from . import _base, _lib
 class ARC4(_base.StreamCipher):
     _algorithm = _lib.ARC4
     name = 'ARC4'
+
+
+class ChaCha20(_base.StreamCipher):
+    _algorithm = _lib.ChaCha20
+    name = 'ChaCha20'
