25c179950146f4889a410a5bd4465f861b2ee3b1
xymy <thyfan@163.com>
2020-03-19 10:00:49 +0800

    Improve stream cipher

--- src/xycrypto/ciphers/__init__.py
+++ src/xycrypto/ciphers/__init__.py
@@ -1,4 +1,6 @@
-from ._stream import ARC4               # NOQA; isort:skip
+from ._stream import (                  # NOQA; isort:skip
+    RC4, ChaCha20
+)
 from ._block_AES import (               # NOQA; isort:skip
     AES_ECB, AES_CBC, AES_OFB, AES_CFB, AES_CTR
 )
@@ -7,7 +9,9 @@ from ._block_TripleDES import (         # NOQA; isort:skip
 )
 
 __all__ = [
-    'ARC4',
+    'ARC4', 'ChaCha20', 'RC4',
     'AES_ECB', 'AES_CBC', 'AES_OFB', 'AES_CFB', 'AES_CTR',
     'TripleDES_ECB', 'TripleDES_CBC', 'TripleDES_OFB', 'TripleDES_CFB', 'TripleDES_CTR'
 ]
+
+ARC4 = RC4                              # alias
