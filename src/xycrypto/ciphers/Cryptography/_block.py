1a14074b14a69e473b09c6c6d5bc60cba0e7bd98
xymy <thyfan@163.com>
2020-03-28 11:42:55 +0800

    Merge _block_*.py

--- src/xycrypto/ciphers/Cryptography/_block.py
+++ src/xycrypto/ciphers/Cryptography/_block.py
@@ -1,6 +1,43 @@
 from . import _base, _lib
 
 
+# AES
+
+
+class _AES(_base.BlockCipher):
+    _algorithm = _lib.AES
+    name = 'AES'
+    block_size = 16
+    key_sizes = frozenset([16, 24, 32])
+
+
+class AES(_AES):
+    pass
+
+
+class AES_ECB(_base.BlockCipherECB, _AES):
+    pass
+
+
+class AES_CBC(_base.BlockCipherCBC, _AES):
+    pass
+
+
+class AES_CFB(_base.BlockCipherCFB, _AES):
+    pass
+
+
+class AES_OFB(_base.BlockCipherOFB, _AES):
+    pass
+
+
+class AES_CTR(_base.BlockCipherCTR, _AES):
+    pass
+
+
+# 3DES
+
+
 class _TripleDES(_base.BlockCipher):
     _algorithm = _lib.TripleDES
     name = '3DES'
